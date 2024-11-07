3bc1f2e9439f7d10199b8cf5cfce53421638006a
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 10:03:08 -0300

    Update Main.java

--- Builder/ex2/Main.java
+++ Builder/ex2/Main.java
@@ -2,8 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         Diretor diretor = new Diretor();
+        // Construindo um Carro
         Veiculo carro = diretor.construirCarro("Fusca", 2);
         System.out.println("Veículo tipo: " + carro.getTipo() + ", Modelo: " + carro.getModelo() + ", Número de portas: " + carro.getNumeroDePortas());
+        // Construindo uma Moto
         Veiculo moto = diretor.construirMoto("Honda PCX");
         System.out.println("Veículo tipo: " + moto.getTipo() + ", Modelo: " + moto.getModelo() + ", Sem número de portas");
     }
