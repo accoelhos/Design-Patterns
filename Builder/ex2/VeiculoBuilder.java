29e3fda3ab93e4e6f8196b967ee11b2a2d447444
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 10:01:04 -0300

    Update VeiculoBuilder.java

--- Builder/ex2/VeiculoBuilder.java
+++ Builder/ex2/VeiculoBuilder.java
@@ -1,29 +1,8 @@
-//builder
-public class VeiculoBuilder {
-    private String tipo;
-    private String modelo;
-    private Integer numeroDePortas;
-
-    public VeiculoBuilder setTipo(String tipo) {
-        this.tipo = tipo;
-        return this;
-    }
-
-    public VeiculoBuilder setModelo(String modelo) {
-        this.modelo = modelo;
-        return this;
-    }
-
-    public VeiculoBuilder setNumeroDePortas(Integer numeroDePortas) {
-        this.numeroDePortas = numeroDePortas;
-        return this;
-    }
-
-    public Veiculo build() {
-        Veiculo veiculo = new Veiculo();
-        veiculo.tipo = this.tipo;
-        veiculo.modelo = this.modelo;
-        veiculo.numeroDePortas = this.numeroDePortas;
-        return veiculo;
+//interface 
+public interface VeiculoBuilder {
+        VeiculoBuilder setTipo(String tipo);
+        VeiculoBuilder setModelo(String modelo);
+        VeiculoBuilder setNumeroDePortas(Integer numeroDePortas);
+        Veiculo build();
     }
 }
