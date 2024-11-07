9a5b601d77b0bae45259cf073409ec42da83fad0
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 11:29:47 -0300

    Update PortaAberta.java

--- State/src/PortaAberta.java
+++ State/src/PortaAberta.java
@@ -3,12 +3,12 @@ public class PortaAberta implements EstadoPorta{
     @Override
     public void abrir(Porta porta) {
         System.out.println("A porta já está aberta.");
-        porta.setEstadoAtual(new PortaAberta());
     }
     //mudando de estado
     @Override
     public void fechar(Porta porta) {
         System.out.println("Fechando a porta...");
+        porta.setEstadoAtual(new PortaFechada());
     }
     
 }
