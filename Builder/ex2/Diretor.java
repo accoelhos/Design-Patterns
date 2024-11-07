0d53b40595a4b4d2b065bc000ad89f9bf870c3b3
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 10:02:38 -0300

    Update Diretor.java

--- Builder/ex2/Diretor.java
+++ Builder/ex2/Diretor.java
@@ -1,16 +1,17 @@
 //diretor 
 public class Diretor {
-    private VeiculoBuilder builder;
-
-    public Diretor() {
-        this.builder = new VeiculoBuilder();
-    }
-
     public Veiculo construirCarro(String modelo, int numeroDePortas) {
-        return builder.setTipo("Carro").setModelo(modelo).setNumeroDePortas(numeroDePortas).build();
+        return new Veiculo.Builder()
+                .setTipo("Carro")
+                .setModelo(modelo)
+                .setNumeroDePortas(numeroDePortas)
+                .build();
     }
-
     public Veiculo construirMoto(String modelo) {
-        return builder.setTipo("Moto").setModelo(modelo).setNumeroDePortas(null).build();
+        return new Veiculo.Builder()
+                .setTipo("Moto")
+                .setModelo(modelo)
+                .setNumeroDePortas(null) 
+                .build();
     }
 }
