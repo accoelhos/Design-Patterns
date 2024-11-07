b8578daa02624dc02f877024e5d3d8b2d9877b78
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 10:01:56 -0300

    Update Veiculo.java

--- Builder/ex2/Veiculo.java
+++ Builder/ex2/Veiculo.java
@@ -1,7 +1,14 @@
 public class Veiculo {
-    private String tipo;
-    private String modelo;
-    private Integer numeroDePortas;
+    private String tipo;  // Carro ou Moto
+    private String modelo; // Modelo do veículo
+    private Integer numeroDePortas; // Número de portas (apenas para carros)
+
+    // Construtor privado, usado apenas pelo Builder
+    private Veiculo(String tipo, String modelo, Integer numeroDePortas) {
+        this.tipo = tipo;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
 
     public String getTipo() {
         return tipo;
@@ -13,5 +20,29 @@ public class Veiculo {
 
     public Integer getNumeroDePortas() {
         return numeroDePortas;
+    }
+     public static class Builder implements VeiculoBuilder {
+        private String tipo;
+        private String modelo;
+        private Integer numeroDePortas;
+
+        public Builder setTipo(String tipo) {
+            this.tipo = tipo;
+            return this;
+        }
+
+        public Builder setModelo(String modelo) {
+            this.modelo = modelo;
+            return this;
+        }
+
+        public Builder setNumeroDePortas(Integer numeroDePortas) {
+            this.numeroDePortas = numeroDePortas;
+            return this;
+        }
+
+        public Veiculo build() {
+            return new Veiculo(tipo, modelo, numeroDePortas);
+        }
     }
 }
