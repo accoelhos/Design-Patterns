956beb3601d2856dd37ce8347d316e33cfa700b7
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-07 09:22:36 -0300

    Create Veiculo.java

---
+++ Builder/ex2/Veiculo.java
@@ -0,0 +1,17 @@
+public class Veiculo {
+    private String tipo;
+    private String modelo;
+    private Integer numeroDePortas;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public Integer getNumeroDePortas() {
+        return numeroDePortas;
+    }
+}
