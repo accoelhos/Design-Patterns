127a0f96d6366dfc10f412ea3bde33e77030ea59
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2024-11-21 13:28:35 +0000

    feat: update proxy

--- Proxy/src/RealDatabase.java
+++ Proxy/src/RealDatabase.java
@@ -1,9 +1,9 @@
 public class RealDatabase implements Database {
     public RealDatabase() {
-        // Simula uma conexão lenta, como se fosse a inicialização do banco de dados.
+        //como se fosse a inicialização do banco de dados
         System.out.println("Conectando ao banco de dados...");
         try {
-            Thread.sleep(2000); // Pausa de 2 segundos para simular o tempo de conexão.
+            Thread.sleep(2000); //2 segundos p simular o tempo de conexão
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
